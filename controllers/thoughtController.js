aed71b7b5ac94eafb198aaabc2c3d4a8be99af80
tskinner429 <tskinner429@gmail.com>
2023-06-01 15:34:37 -0400

    set up thoughtController layout and filled in thoughtRoutes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,27 @@
+const { User, Thought } = require("../models")
+
+const thoughtController = {
+    getThoughts(req, res) {
+
+    },
+    getOneThought(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    addReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    }
+}
+
+module.exports = thoughtController;
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router()
 
 const {
-
+    getThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
 } = require("../../controllers/thoughtController")
 
-router.route("/").get().post()
-router.route("/:id").get().put().delete()
-router.route("/:id/reactions").post()
-router.route("/:id/reactions/:reactionId").delete()
+router.route("/").get(getThoughts).post(createThought)
+router.route("/:id").get(getOneThought).put(updateThought).delete(deleteThought)
+router.route("/:id/reactions").post(addReaction)
+router.route("/:id/reactions/:reactionId").delete(deleteReaction)
 
 module.exports = router;
