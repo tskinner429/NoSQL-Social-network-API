e99b79650b5678c62a109579373bd0b525544130
tskinner429 <tskinner429@gmail.com>
2023-06-01 15:32:51 -0400

    set up userController layout and filled in userRoutes

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const { User } = require("../models")
+
+const userController = {
+    getUsers(req, res) {
+
+    },
+    getOneUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    deleteFriend(req, res) {
+
+    }
+}
+
+module.exports = userController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router()
 
 const {
-
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
 } = require("../../controllers/userController")
 
-router.route("/").get().post()
-router.route("/:id").get().put().delete()
-router.route("/:id/friends/:friendId").post().delete()
+router.route("/").get(getUsers).post(createUser)
+router.route("/:id").get(getOneUser).put(updateUser).delete(deleteUser)
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend)
 
 module.exports = router;
